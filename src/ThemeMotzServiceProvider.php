a965a1024894384717e8b5a5fb11ae7039071c8b
xemphimhay <lanhp871@gmail.com>
2024-02-28 13:08:54 +0700

    Update ThemeMotzServiceProvider.php

--- src/ThemeMotzServiceProvider.php
+++ src/ThemeMotzServiceProvider.php
@@ -96,9 +96,7 @@ class ThemeMotzServiceProvider extends ServiceProvider
                         'hint' => 'Label|relation|find_by_field|value|sort_by_field|sort_algo|limit|show_template (top_thumb|top_trending)',
                         'value' => <<<EOT
                         Trending|trending|||||6|top_trending
-                        Top phim lẻ||type|single|view_week|desc|6|top_thumb
-                        Top phim bộ||type|series|view_week|desc|6|top_thumb
-                        Bảng xếp hạng||is_copyright|0|view_week|desc|6|top_thumb
+                        Phim Sắp Chiếu||status|trailer|view_week|desc|6|top_upcomming
                         EOT,
                         'attributes' => [
                             'rows' => 5
